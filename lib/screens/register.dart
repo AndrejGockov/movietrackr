54317a6548871403c37bc94bfe3311990d99c2c1
Andrej Gockov <gockovandrej@gmail.com>
2026-05-09 17:02:40 +0200

    Update register.dart

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -211,18 +211,4 @@ class _RegisterState extends State<Register> {
       ),
     );
   }
-
-  // InputDecoration inputDecoration(String label, IconData icon) {
-  //   return InputDecoration(
-  //     hintText: label,
-  //     hintStyle: AppTheme.h4SemiboldOnMediumBlue,
-  //     border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-  //     prefixIcon: Icon(icon, color: AppTheme.lightBlue),
-  //     focusedBorder: OutlineInputBorder(
-  //       borderSide: BorderSide(color: AppTheme.lightBlue),
-  //       borderRadius: BorderRadius.circular(AppTheme.md),
-  //     ),
-  //     errorStyle: AppTheme.h6SemiboldPrimaryRed,
-  //   );
-  // }
 }
